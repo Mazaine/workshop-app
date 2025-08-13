1ca4c84263186774677f9484535b3d729d27f8e4
Mazaine <68555165+Mazaine@users.noreply.github.com>
2025-08-13 19:26:10 +0200

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -3,18 +3,20 @@ document.write(`
     <div class="container mx-auto px-4 py-3 flex justify-between items-center">
         <h1 class="text-2xl font-bold">Egér Alkatrész Kezelő</h1>
         
-        <!-- Hamburger ikon mobilra -->
-        <button id="menu-toggle" class="block md:hidden focus:outline-none text-3xl">
-            ☰
-        </button>
+        <div class="flex items-center">
+            <!-- Hamburger ikon mobilra -->
+            <button id="menu-toggle" class="block md:hidden focus:outline-none text-3xl ml-4">
+                ☰
+            </button>
 
-        <!-- Menü gombok -->
-        <div id="menu-links" class="overflow-hidden max-h-0 md:max-h-none transition-all duration-500 ease-in-out md:flex md:space-x-4 flex-col md:flex-row mt-4 md:mt-0">
-            <button onclick="window.location.href='add-part.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Alkatrész hozzáadása</button>
-            <button onclick="window.location.href='parts-list.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Alkatrész lista</button>
-            <button onclick="window.location.href='order.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Rendelések</button>
-            <button onclick="window.location.href='create-workorder.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Munkalap generálása</button>
-            <button onclick="window.location.href='workorders-list.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Munkalapok</button>
+            <!-- Menü gombok -->
+            <div id="menu-links" class="overflow-hidden max-h-0 md:max-h-none transition-all duration-500 ease-in-out md:flex md:space-x-4 flex-col md:flex-row mt-4 md:mt-0 ml-4">
+                <button onclick="window.location.href='add-part.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Alkatrész hozzáadása</button>
+                <button onclick="window.location.href='parts-list.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Alkatrész lista</button>
+                <button onclick="window.location.href='order.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Rendelések</button>
+                <button onclick="window.location.href='create-workorder.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Munkalap generálása</button>
+                <button onclick="window.location.href='workorders-list.html'" class="px-4 py-2 bg-blue-700 rounded-lg hover:bg-blue-800">Munkalapok</button>
+            </div>
         </div>
     </div>
 </nav>
